c474dd19f6169eee42509d4f3c407823407e8218
Anton Shelestov <shelestov.a.s@gmail.com>
2017-04-15 14:05:30 +0300

    загрузка исходников

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/*
+* //= ../../node_modules/jquery/dist/jquery.min.js
+* //= ../../node_modules/tether/dist/js/tether.min.js
+* //= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
+* //= ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js
+* //= ../../node_modules/swiper/dist/js/swiper.jquery.min.js
+*
+* //= partials/app.js
+*/
